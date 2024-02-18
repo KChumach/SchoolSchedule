e88cc0be0502c2ee3017487d10dd34cea716a61a
kiril <kiril@DESKTOP-OAI7IAV>
2024-02-18 17:08:22 +0300

    Добавьте файлы проекта.

---
+++ Interfaces/IRepository.cs
@@ -0,0 +1,10 @@
+﻿using SchoolSchedule.Entities;
+
+namespace SchoolSchedule.Interfaces
+{
+    internal interface IRepository<TEntity, TFilter>
+    {
+        Task<IEnumerable<TEntity>> GetByQueryAsync(TFilter filter, CancellationToken token = default);
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using Telegram.Bot.Polling;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Exceptions;
+using SchoolSchedule;
+
+try
+{
+    var factory = Factory.GetInstance();
+    Console.WriteLine("Запущен бот " + factory.Bot.GetMeAsync().Result.FirstName);
+
+    var cts = new CancellationTokenSource();
+    var cancellationToken = cts.Token;
+    var receiverOptions = new ReceiverOptions
+    {
+        AllowedUpdates = { },
+    };
+    factory.Bot.StartReceiving(
+        factory.TGService.HandleUpdateAsync,
+        factory.TGService.HandleErrorAsync,
+        receiverOptions,
+        cancellationToken
+    );
+}
+catch (Exception ex)
+{
+    Console.WriteLine( ex.ToString() );
+}
+
+
+Console.ReadLine();
